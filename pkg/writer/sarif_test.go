054dcca6ee494b0a06ebf3733510eca1e9f38a45
Shaopeng Li <v-shaopengli@microsoft.com>
2022-09-19 00:59:34 -0700

    Fix Test 1

--- pkg/writer/sarif_test.go
+++ pkg/writer/sarif_test.go
@@ -18,60 +18,61 @@ var testpath = fmt.Sprintf("file://%s", abstestpath)
 var testpathForGH = violationsInput.Violations[0].File
 
 const violationTemplate = `{
-          "version": "2.1.0",
-          "$schema": "https://json.schemastore.org/sarif-2.1.0-rtm.5.json",
-          "runs": [
+  "version": "2.1.0",
+  "$schema": "https://json.schemastore.org/sarif-2.1.0-rtm.5.json",
+  "runs": [
+    {
+      "tool": {
+        "driver": {
+          "informationUri": "https://github.com/tenable/terrascan",
+          "name": "terrascan",
+          "rules": [
             {
-              "tool": {
-                "driver": {
-                  "name": "terrascan",
-                  "version": "%s",
-                  "informationUri": "https://github.com/tenable/terrascan",
-                  "rules": [
-                    {
-                      "id": "AWS.S3Bucket.DS.High.1043",
-                      "name": "s3EnforceUserACL",
-                      "shortDescription": {
-                        "text": "S3 bucket Access is allowed to all AWS Account Users."
-                      },
-                      "properties": {
-                        "category": "S3",
-                        "severity": "HIGH"
-                      }
-                    }
-                  ]
+              "id": "AWS.S3Bucket.DS.High.1043",
+              "name": "s3EnforceUserACL",
+              "shortDescription": {
+                "text": "S3 bucket Access is allowed to all AWS Account Users."
+              },
+              "properties": {
+                "category": "S3",
+                "severity": "HIGH"
+              }
+            }
+          ],
+          "version": "%s"
+        }
+      },
+      "results": [
+        {
+          "ruleId": "AWS.S3Bucket.DS.High.1043",
+          "ruleIndex": 0,
+          "level": "error",
+          "message": {
+            "text": "S3 bucket Access is allowed to all AWS Account Users."
+          },
+          "locations": [
+            {
+              "physicalLocation": {
+                "artifactLocation": {
+                  "uri": "%s"
+                },
+                "region": {
+                  "startLine": 20
                 }
               },
-              "results": [
+              "logicalLocations": [
                 {
-                  "ruleId": "AWS.S3Bucket.DS.High.1043",
-                  "level": "error",
-                  "message": {
-                    "text": "S3 bucket Access is allowed to all AWS Account Users."
-                  },
-                  "locations": [
-                    {
-                      "physicalLocation": {
-                        "artifactLocation": {
-                          "uri": "%s"
-                        },
-                        "region": {
-                          "startLine": 20
-                        }
-                      },
-                      "logicalLocations": [
-                        {
-                          "name": "bucket",
-                          "kind": "aws_s3_bucket"
-                        }
-                      ]
-                    }
-                  ]
+                  "name": "bucket",
+                  "kind": "aws_s3_bucket"
                 }
               ]
             }
           ]
-        }`
+        }
+      ]
+    }
+  ]
+}`
 
 var expectedSarifOutput1 = fmt.Sprintf(violationTemplate, version.GetNumeric(), testpath)
 
